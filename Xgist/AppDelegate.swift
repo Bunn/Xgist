a1b5d4a2d9aba82a8464c87e0d30d933d18a1381
Fernando Bunn <bunn@idevzilla.com>
2016-12-12 09:28:40 -0200

    Initial commit

---
+++ Xgist/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Xgist
+//
+//  Created by Fernando Bunn on 10/12/16.
+//  Copyright © 2016 Fernando Bunn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
