48a4527c1d4c1ddd5b47d98768b0478ca22c6465
Fernando Bunn <bunn@idevzilla.com>
2017-03-28 22:33:32 -0300

    Refactor API code
    Moved all API code to different GitHub related files to make it easier to call other methods like “authenticate” to post authenticated gists in the future.

---
+++ GitHubRouter.swift
@@ -0,0 +1,56 @@
+//
+//  GitHubRouter.swift
+//  Xgist
+//
+//  Created by Fernando Bunn on 27/03/17.
+//  Copyright © 2017 Fernando Bunn. All rights reserved.
+//
+
+import Foundation
+
+enum GitHubRouter {
+    case auth([String : Any])
+    case gists([String : Any])
+    
+    var basePath: String {
+        return "https://api.github.com"
+    }
+    
+    var path: String {
+        switch self {
+        case .auth:
+            return "authorizations/clients/\(GitHubCredential.clientID.rawValue)/wololo" //needs to return unique hash instead of "wololo"
+        case .gists:
+            return "gists"
+        }
+    }
+    
+    var method: String {
+        switch self {
+        case .auth:
+            return "PUT"
+        case .gists:
+            return "POST"
+        }
+    }
+    
+    var httpBody: Data? {
+        switch self {
+        case .auth(let params), .gists(let params):
+            let jsonData = try? JSONSerialization.data(withJSONObject: params)
+            return jsonData
+        }
+    }
+    
+    var request: URLRequest? {
+        guard let url = URL(string: "\(basePath)/\(path)") else {
+            print("Invalid URL")
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        request.httpBody = httpBody
+        return request
+    }
+}
